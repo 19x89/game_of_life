6e33bf67804e7336e534451cf0032fcad3db60f5
19x89 <33625931+19x89@users.noreply.github.com>
2022-09-26 15:06:20 +0200

    Update GameOfLifeTest.java

--- GameOfLifeTest.java
+++ GameOfLifeTest.java
@@ -1,5 +1,9 @@
 package game_of_life;
 
+/*
+Die Hauptklasse ist zum Starten des Programms.
+*/
+
 public class GameOfLifeTest {
     public static void main( String[] args ) {
         Welt neueWelt = new Welt( 20 );
